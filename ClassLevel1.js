68b361bda1ca4133d344915d6fb67e17733e3fd4
Siddharth Chaudhary <53467812+sid4991@users.noreply.github.com>
2022-11-03 11:53:10 -0700

    Update ClassLevel1.js

--- ClassLevel1.js
+++ ClassLevel1.js
@@ -269,3 +269,39 @@ Export.image.toDrive({
     maxPixels: 1e9
 })
 
+
+var roi = /* color: #00ffff */ee.Geometry.Point([-117.14937090735943, 46.72281677669114]);
+//Exporting Charts and Images
+// Import the Landsat 8 TOA image collection.
+var l8 = ee.ImageCollection('LANDSAT/LC08/C01/T1_TOA');
+
+// Map a function over the Landsat 8 TOA collection to add an NDVI band.
+var withNDVI = l8.map(function(image) {
+  var ndvi = image.normalizedDifference(['B5', 'B4']).rename('NDVI');
+  return image.addBands(ndvi);
+});
+
+// Create a chart.
+var chart = ui.Chart.image.series({
+  imageCollection: withNDVI.select('NDVI'),
+  region: roi,
+  reducer: ee.Reducer.first(), //if it was polygon prefer mean to avoid cloud masking
+  scale: 30
+}).setOptions({title: 'NDVI over time'});
+
+// Display the chart in the console.
+print(chart);
+
+// Earth Engine includes a cloud-masking algorithm for Landsat sensors with a thermal band: 
+//ee.Algorithms.Landsat.simpleCloudScore()
+
+var cloudlessNDVI = l8.map(function(image) {
+  // Get a cloud score in [0, 100].
+  var cloud = ee.Algorithms.Landsat.simpleCloudScore(image).select('cloud');
+  // Create a mask of cloudy pixels from an arbitrary threshold.
+  var mask = cloud.lte(20);
+  // Compute NDVI.
+  var ndvi = image.normalizedDifference(['B5', 'B4']).rename('NDVI');
+  // Return the masked image with an NDVI band.
+  return image.addBands(ndvi).updateMask(mask);
+});
