b936eb280b0bd10e3fde5accb7ea2daa06ce7139
sid4991 <53467812+sid4991@users.noreply.github.com>
2022-11-07 13:29:38 -0800

    initial

--- Applications.js
+++ Applications.js
@@ -163,3 +163,47 @@ var threshold = 0.3
 // Display Burned Areas
 var burned = change.gt(threshold)
 Map.addLayer(burned, {min:0, max:1, palette: ['white', 'red']}, 'Burned', false) 
+
+
+// LandCover
+// Make a cloud-free Landsat 8 TOA composite (from raw imagery).
+var points = ee.FeatureCollection('GOOGLE/EE/DEMOS/demo_landcover_labels');
+var l8 = ee.ImageCollection('LANDSAT/LC08/C01/T1');
+
+var image = ee.Algorithms.Landsat.simpleComposite({
+  collection: l8.filterDate('2018-01-01', '2018-12-31'),
+  asFloat: true
+});
+
+print(image)
+
+// Use these bands for prediction.
+var bands = ['B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B10', 'B11'];
+
+// Load training points. The numeric property 'class' stores known labels.
+
+
+// This property stores the land cover labels as consecutive
+// integers starting from zero.
+var label = 'landcover';
+
+// Overlay the points on the imagery to get training.
+var training = image.select(bands).sampleRegions({
+  collection: points,
+  properties: [label],
+  scale: 30
+});
+
+// Train a CART classifier with default parameters.
+var trained = ee.Classifier.smileCart().train(training, label, bands);
+
+// Classify the image with the same bands used for training.
+var classified = image.select(bands).classify(trained);
+
+// Display the inputs and the results.
+Map.centerObject(points, 11);
+Map.addLayer(image, {bands: ['B4', 'B3', 'B2'], max: 0.4}, 'image');
+Map.addLayer(classified,
+             {min: 0, max: 2, palette: ['red', 'green', 'blue']},
+             'classification');
+Map.addLayer(points);
